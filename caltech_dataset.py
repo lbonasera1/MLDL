32d9d43a90f1d9f0b581498b87c96788d00a9cf3
lbonasera1 <64270247+lbonasera1@users.noreply.github.com>
2020-04-24 18:24:50 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -13,14 +13,38 @@ def pil_loader(path):
         img = Image.open(f)
         return img.convert('RGB')
 
+def make_dataset(directory, class_to_idx, split):
+    instances = []
+    directory = os.path.expanduser(directory)
+  
+    for target_class in sorted(class_to_idx.keys()):
+        class_index = class_to_idx[target_class]
+        target_dir = os.path.join(directory, target_class)
+        if not os.path.isdir(target_dir):
+            continue
+        for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
+            for fname in sorted(fnames):
+                path = os.path.join(root, fname)
+                item = path, class_index
+                instances.append(item)
+    return instances
 
 class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
-
+        self.split = split + '.txt' # This defines the split you are going to use
+                                    # (split files are called 'train.txt' and 'test.txt')
+        
+        directory = os.path.expanduser(root)
+        file_dir = os.path.join(directory, split)
+        file = open(file_dir, 'r')
+        self.samples_to_get = file.readlines()
+        for f in self.samples_to_get:
+            if (f.
+           
+        
+        
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
         - If the RAM size allows it, it is faster to store all data in memory
@@ -30,6 +54,15 @@ class Caltech(VisionDataset):
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
 
+        
+    def _find_classes(self, dir):
+        
+        classes = [d.name for d in os.scandir(dir) if d.is_dir()]
+        classes.remove('BACKGROUND_Google')
+        classes.sort()
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        return classes, class_to_idx
+        
     def __getitem__(self, index):
         '''
         __getitem__ should access an element through its index
